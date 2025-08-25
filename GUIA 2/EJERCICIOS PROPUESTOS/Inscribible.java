2b9e9c780c42e3fbf9a0e7ec960edb218971e53a
Rogix34 <roy.santos@ucsm.edu.pe>
2025-08-24 22:18:41 -0500

    Delete GUIA 2/EJERCICIOS PROPUESTOS directory

--- GUIA 2/EJERCICIOS PROPUESTOS/Inscribible.java
+++
@@ -1,9 +0,0 @@
-/**
- * Interfaz: permite POLIMORFISMO de tipos inscribibles.
- * Un Curso es "Inscribible".
- */
-public interface Inscribible {
-    boolean agregarEstudiante(Estudiante e);
-    boolean removerEstudiante(Estudiante e);
-    boolean tieneCupos();
-}
